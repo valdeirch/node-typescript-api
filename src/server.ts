da44058c5afe5f8873b5bc292359fbdafe53ce32
FS6232 <valdeir.chaves@mouts.info>
2022-01-22 15:01:50 -0300

    prettier

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,31 @@
-import './util/module-alias'
-import { Server } from '@overnightjs/core'
-import bodyParser from 'body-parser'
-import { ForecastController } from './controllers/forecast'
-import { Application } from 'express'
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
-
   constructor(private port = 3000) {
     // super para server
-    super()
+    super();
   }
 
   public init(): void {
-    this.setupExpress()
-    this.setupControllers()
+    this.setupExpress();
+    this.setupControllers();
   }
 
   private setupExpress(): void {
     // vem do server
-    this.app.use(bodyParser.json())
+    this.app.use(bodyParser.json());
   }
 
   private setupControllers(): void {
-    const forecastController = new ForecastController()
-    this.addControllers([forecastController])
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
   }
 
   public getApp(): Application {
-    return this.app
+    return this.app;
   }
 }
