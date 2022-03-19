807cab9f5f3c4b7d2b03a79a6d00bef669e2d3e2
FS6232 <valdeir.chaves@mouts.info>
2022-03-19 09:34:28 -0300

    [C05P01] - Módulo de Users - Criando Controller e Model

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -26,8 +27,13 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
+    const usersController = new UsersController();
 
-    this.addControllers([forecastController, beachesController]);
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   public getApp(): Application {
